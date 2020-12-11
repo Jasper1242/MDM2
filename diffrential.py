be1869f09812a3459097a42f5e0cb5b54098d39e
Jasper Morgan <jaspiejmorgan@gmail.com>
2020-12-11 15:31:23 +0000

    Update diffrential.py

--- diffrential.py
+++ diffrential.py
@@ -9,26 +9,32 @@ import math
 
 
 
-"my'' + by'' + ky = Fo(cos((2pi*v/xgap)t)"
-"z = y' == mz' + bz + ky =  Fo(cos((2pi*v/xgap)t)" \
+"mz'' + cz' + kz = 4kh + mg"
+
 "this represents a 2nd order diffrential as a first order one so that the odeint function can be used"
 
-'constants:'
-m = 1
+"constants:"
+mL = 386007
+mU = 159207
 b = 1
 k = 1
 
-def dU_dt(U, t):
-    "variables:"
-    xgap = 1.0
-    Fo = 1.0
-    v = 10.0
 
-    "y=U[0] and z=U[1] this function returns [y', z']"
-    return [b * U[1], (-k*U[0]) + (-m*U[1]) + (np.cos((((2*math.pi)*v)/xgap)*t) * Fo)]
+def dU_dt(U, t = 1):
+    "variables:"
+    print("t", t)
+    mg = 9.8 * mL
+    h = 0.99
+    c =1
+    "h = np.sin(0.0034/int(t))"
+
+
+    "y = z'"
+    "z=U[0] and y=U[1] this function returns [z', y']"
+    return [c * U[1], - (-4*k*U[0]) - (-mL*U[1]) + (4*k*np.sin(2)) + mg]
 
 U0 = [0, 0]
-ts = np.linspace(0, 2, 200)
+ts = np.linspace(1, 2, 200)
 Us = odeint(dU_dt, U0, ts)
 ys = Us[:, 0]
 
